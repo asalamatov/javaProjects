57e58c3beb4159dabf9200488e6b913c99372d56
asalamatov <azamat.salamatov2004@gmail.com>
2023-02-28 21:08:21 -0600

    hangman almost done, but with bugs

--- javapractice/hangman/Hangman.java
+++ javapractice/hangman/Hangman.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
+//import java.util.Random;
 
 public class Hangman {
     
@@ -21,7 +21,7 @@ public class Hangman {
     private static BufferedReader bufferedFileReader;
 
     public Hangman() throws IOException{
-        initializeStreams;
+        initializeStreams();
         mysteryWord = pickWord();
         currentGuess = initializeCurrentGuess();
     }
@@ -34,16 +34,20 @@ public class Hangman {
             String currentLine = bufferedFileReader.readLine();
             while (currentLine!=null){
                 dictionary.add(currentLine);
+                currentLine = bufferedFileReader.readLine();
             }
             bufferedFileReader.close();
             fileReader.close();
         }catch(IOException e){
             System.out.println("Could not init streams");
         }
+    }
+
 
         public String pickWord() {
-            Random rand = new Random();
-            int wordIndex = Math.abs(rand.nextInt()) % dictionary.size();
+            //Random rand = new Random();
+            //int wordIndex = Math.abs(rand.nextInt()) % dictionary.size();
+            int wordIndex = 5;
             return dictionary.get(wordIndex);
         }
 
@@ -57,11 +61,34 @@ public class Hangman {
                 }
             }
             return current;
-        }
+        
+    }
+
+    public String getFormalCurrentGuess(){
+        return "Current Guess: " + currentGuess.toString();
+    }
+
+    public boolean gameOver(){
+        return true;
     }
 
     public String drawPicture(){
-
+        switch(currentTry){
+            case 0: 
+            return ("\n    +---+\n    |   |\n        |\n        |\n        |\n        |\n  =========\n");
+            case 1: 
+            return ("\n    +---+\n    |   |\n    O   |\n        |\n        |\n        |\n  =========\n");
+            case 2:
+            return ("\n    +---+\n    |   |\n    O   |\n    |   |\n        |\n        |\n  =========\n");
+            case 3:
+            return ("\n    +---+\n    |   |\n    O   |\n   /|   |\n        |\n        |\n  =========\n");
+            case 4:
+            return ("\n    +---+\n    |   |\n    O   |\n   /|\\  |\n        |\n        |\n  =========\n");
+            case 5:
+            return ("\n    +---+\n    |   |\n    O   |\n   /|\\  |\n   /    |\n        |\n  =========\n");
+            default:
+            return ("\n    +---+\n    |   |\n    O   |\n   /|\\  |\n   / \\  |\n        |\n  =========\n");
+        }
     }
 
 }
--- javapractice/hangman/HangmanApplication.java
+++ javapractice/hangman/HangmanApplication.java
@@ -1,35 +1,45 @@
 package hangman;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class HangmanApplication {
     
 
 
-    public static void main(String[] args){
-        //how do we play the game
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to hangman! I will pick a word and you will try to geyss it character by character"
-        + ". If you guess wrong 6 times, then I win. If you can guess it before them, you win. "
-        + "Are you ready? I hope so because I am.");
-        System.out.println();
-        System.out.println("I have picked my word. Below is a picture, and below "
-        + "that is your current guess, which starts off as nothing. Every time you "
-        + "guess incorrectly. I add a body part to the picture. When there is a full"
-        + " person, you lose");
-
-        //allows for multiple games
-        boolean doYouWantToPlay = true;
-        while(doYouWantToPlay){
-            //setting up the game
-
-            while (!game.gameOver()){
-                //playing the game
-            }
+    public static void main(String[] args) throws IOException{
+        try (//how do we play the game
+        Scanner sc = new Scanner(System.in)) {
+            System.out.println("Welcome to hangman! I will pick a word and you will try to geyss it \ncharacter by character"
+            + ". \nIf you guess wrong 6 times, then I win. \nIf you can guess it before them, you win. "
+            + "\nAre you ready? I hope so because I am.");
             System.out.println();
-            System.out.println("Do you want to play another game? Enter Y if you do.");
-            Character response = (sc.next().toUpperCase()).charAt(0);
-            doYouWantToPlay = (response=='Y');
+            System.out.println("I have picked my word. Below is a picture, and below "
+            + "\nthat is your current guess, which starts off as nothing. Every time you "
+            + "\nguess incorrectly. I add a body part to the picture. When there is a full"
+            + " \nperson, you lose");
+
+            //allows for multiple games
+            boolean doYouWantToPlay = true;
+            while(doYouWantToPlay){
+                //setting up the game
+                System.out.println("Alright! Let's play.");
+                Hangman game = new Hangman();
+
+                do{
+                    System.out.println();
+                    System.out.println(game.drawPicture());
+                    System.out.println();
+                    System.out.println(game.getFormalCurrentGuess());
+                    System.out.println(game.mysteryWord);
+
+                }while (!game.gameOver());
+
+                System.out.println();
+                System.out.println("Do you want to play another game? Enter Y if you do.");
+                Character response = (sc.next().toUpperCase()).charAt(0);
+                doYouWantToPlay = (response=='Y');
+            }
         }
     }
 }
